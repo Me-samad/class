f37189e7d4c190c4d2f5e6778c4cac4a1936650d
Abdul Samad <bhaicharlie6@gmail.com>
2024-11-20 16:32:30 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+echo("PHP Ab Samad");
+echo ("<h1>PHP samad</h1>");
+ $show = "PHP samad";
+echo("I am Leaning " . $show);
+ echo("<script>alert('php samad');</script>");
+ echo("10");
+ echo(10);
+ $num = 10;
+ $num_02 = 100;
+ echo($num > $num_02);
+
+?>
